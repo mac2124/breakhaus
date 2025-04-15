8aa186b0ace43523d6d48498e167946bca9ea7a8
Maciej Kosela <maciek.2124@gmail.com>
2025-04-15 18:07:43 +0100

    move code to component

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,13 @@
 // pages/index.tsx
 import React from "react";
 import './globals.css';
+import VideoHeader from "@/public/components/VideoHeader";
 
 export default function Home() {
   return (
-    
-  <div>
-    {/* Video element to play the MP4 file */}
-    <video
-      src="public/BH_Looped.mp4"
-      autoPlay
-      loop
-      muted
-      playsInline
-      className="background-video"
-    />
-    
-    {/* Rest of your page content */}
-    <h1 style={{ position: "relative", zIndex: 1 }}>Welcome to Breakhaus</h1>
-    {/* Add other components or content here */}
-  </div>
+    <main>
+      <VideoHeader />
+      {/* Other sections below */}
+    </main>
   );
 }
