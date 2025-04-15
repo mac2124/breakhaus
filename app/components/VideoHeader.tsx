8ea0a10681e980cafa6ae7e80bbf7353cdf31f35
Maciej Kosela <maciek.2124@gmail.com>
2025-04-15 19:25:53 +0100

    fix

--- app/components/VideoHeader.tsx
+++ app/components/VideoHeader.tsx
@@ -13,6 +13,9 @@ const VideoHeader = () => {
         <source src="/breakhaus/assets/BH_Looped.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <div className="absolute bottom-10 left-0 w-full flex items-center justify-center z-10">
+        <h1 className="text-with-noise">Work in Progress</h1>
+      </div>
       <div className="absolute top-0 left-0 w-full h-full bg-black/30 z-0" />
     </div>
   );
