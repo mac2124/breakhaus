e88bf2e164013dacae030d6c466d6bd254ad1834
Maciej Kosela <maciek.2124@gmail.com>
2025-04-15 18:21:49 +0100

    changes

--- public/components/VideoHeader.tsx
+++ public/components/VideoHeader.tsx
@@ -10,7 +10,7 @@ const VideoHeader = () => {
         muted
         playsInline
       >
-        <source src="/videos/header-video.mp4" type="video/mp4" />
+        <source src="/videos/BH_Looped.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="relative z-10 flex items-center justify-center h-full">
