e9b1cad93e27849a292cefb594ce88dacd1b8cbc
Maciej Kosela <maciek.2124@gmail.com>
2025-04-29 15:23:47 +0100

    Add arrow

--- app/components/VideoHeader.tsx
+++ app/components/VideoHeader.tsx
@@ -14,7 +14,10 @@ const VideoHeader = () => {
         Your browser does not support the video tag.
       </video>
       <div className="absolute bottom-10 left-0 w-full flex items-center justify-center z-10">
-        <h1 className="text-with-noise">Work in Progress</h1>
+        <div className="double-arrow animate-fade-in">
+          <div className="arrow"></div>
+          <div className="arrow mt-2"></div>
+        </div>
       </div>
       <div className="absolute top-0 left-0 w-full h-full bg-black/30 z-0" />
     </div>
