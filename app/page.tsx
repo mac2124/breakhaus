5159491fe498fd75341e6843c26449804eb81b41
Maciej Kosela <maciek.2124@gmail.com>
2025-07-06 13:57:53 +0100

    add new page

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,15 @@ import React from "react";
 import './globals.css';
 import VideoHeader from "@/app/components/VideoHeader/VideoHeader";
 import WIPHeader from "@/app/components/WIPHeader/WIPHeader";
+import AboutUsHeader from "./components/AboutUsHeader/AboutUsHeader";
 
 export default function Home() {
   return (
     <main>
       <VideoHeader />
       <WIPHeader />
+      <AboutUsHeader />
+      {/* Add more components or content as needed */}
     </main>
   );
 }
